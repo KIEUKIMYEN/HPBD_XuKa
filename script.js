2b5f3c04ee625206beae07e1ebd3d851b8264dcb
KIEUKIMYEN <iamyen0305@gmail.com>
2024-11-21 17:31:53 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ function checkPassword() {
     const errorMessage = document.getElementById("error-message");
 
     if (inputPassword === correctPassword) {
-        lockScreen.classList.add("hidden");
-        birthdayContent.classList.remove("hidden");
+        lockScreen.classList.add("hidden"); // Ẩn màn hình khóa
+        birthdayContent.classList.remove("hidden"); // Hiển thị nội dung
     } else {
         errorMessage.textContent = "Sai mật khẩu! Thử lại nhé.";
     }
 }
+
